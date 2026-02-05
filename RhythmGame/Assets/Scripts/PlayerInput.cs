b1e3f538768a29624733a8212a60d643f17cf46b
Gianmarco Nagliati Bravi <98173347+naglius98@users.noreply.github.com>
2026-02-04 22:25:32 -0500

    Player input
    - Added player input action for new input system
    - Hardcoded old input for easiness
    - Clicking notes will send a message to console showing a key had been pressed

---
+++ RhythmGame/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    void Update()
+    {
+        if (Keyboard.current.dKey.wasPressedThisFrame) 
+        {
+            CheckHit(0);
+        }
+        if (Keyboard.current.fKey.wasPressedThisFrame) 
+        {
+            CheckHit(1);
+        }
+        if (Keyboard.current.jKey.wasPressedThisFrame) 
+        {
+            CheckHit(2);
+        }
+        if (Keyboard.current.kKey.wasPressedThisFrame) 
+        {
+            CheckHit(3);
+        }
+    }
+
+    void CheckHit(int railIndex)
+    {
+        Debug.Log($"Rail {railIndex} pressed!");
+    }
+}
+
